34d65e80a7c671f2322721cf61232c0c21ba2f7b
John Roman <jrrmod@gmail.com>
2020-04-15 14:11:14 -0400

    adding gitignore, updating the vpc script to have the proper count of availability zones

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,100 +4,111 @@ provider "aws" {
 
 locals {
   product_tags = {
-    "Name"        = "${var.product-name}"
-    "Environment" = "${var.environment}"
+    "Name"        = var.product-name
+    "Environment" = var.environment
   }
 }
 
 data "aws_availability_zones" "available-azs" {
   state                 = "available"
-  blacklisted_zone_ids  = "${var.blacklisted-azs}"
+  blacklisted_zone_ids  = var.blacklisted-azs
+
+  filter {
+    name    = "opt-in-status"
+    values  = ["opt-in-not-required"]
+  }
 }
 
 resource "aws_vpc" "main" {
-  cidr_block            = "${var.vpc-cidr}"
+  cidr_block            = var.vpc-cidr
   enable_dns_hostnames  = true 
 
-  tags = "${local.product_tags}"
+  tags = local.product_tags
 }
 
 resource "aws_subnet" "public" {
-  count                   = "${length(aws_availability_zones.available-azs)}"
-  vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "${cidrsubnet(var.vpc-cidr, 8 count.index)}"
-  availability_zone       = "${element(aws_availability_zones.available-azs, count.index)}"
+  count                   = length(data.aws_availability_zones.available-azs.names)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc-cidr, 8, count.index)
+  availability_zone       = element(data.aws_availability_zones.available-azs.names, count.index)
   map_public_ip_on_launch = true
   
   tags = {
-    "Name" = "Public subnet - ${element(aws_availability_zones.available-azs, count.index)}"
+    "Name" = "Public subnet - ${element(data.aws_availability_zones.available-azs.names, count.index)}"
   }
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-  tags = "${local.product_tags}"
+  tags = local.product_tags
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.main.id}"
-  }
-
-  depends_on = ["${aws_internet_gateway.main.id}"]
+  vpc_id = aws_vpc.main.id
 
   tags = {
     "Name" = "Public Routing Table"
   }
 }
 
+resource "aws_route" "public" {
+  route_table_id              = aws_route_table.public.id
+  destination_cidr_block      = "0.0.0.0/0"
+  gateway_id                  = aws_internet_gateway.main.id
+}
+
 resource "aws_route_table_association" "public" {
-  subnet_id       = "${element(aws_subnet.public.*.id, count.index)}"
-  route_table_id  = "${aws_route_table.public.id}"
+  count           = length(data.aws_availability_zones.available-azs.names)
+  subnet_id       = element(aws_subnet.public.*.id, count.index)
+  route_table_id  = aws_route_table.public.id
 }
 
 resource "aws_subnet" "private" {
-  count                   = "${length(aws_availability_zones.available-azs)}"
-  vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "${cidrsubnet(var.vpc-cidr, 8 count.index)}"
-  availability_zone       = "${element(aws_availability_zones.available-azs, count.index)}"
+  count                   = length(data.aws_availability_zones.available-azs.names)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc-cidr, 8, count.index + length(data.aws_availability_zones.available-azs))
+  availability_zone       = element(data.aws_availability_zones.available-azs.names, count.index)
   map_public_ip_on_launch = false
 
   tags = {
-    "Name" = "Private subnet - ${element(aws_availability_zones.available-azs, count.index)}"
+    "Name" = "Private subnet - ${element(data.aws_availability_zones.available-azs.names, count.index)}"
   }
 }
 
+resource "aws_eip" "nat" {
+  count = length(data.aws_availability_zones.available-azs.names)
+  vpc   = true
+}
+
 resource "aws_nat_gateway" "main" {
-  count         = "${length(aws_availability_zones.available-azs)}"
-  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
-  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  count         = length(data.aws_availability_zones.available-azs.names)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = element(aws_eip.nat.*.id, count.index)
 
   tags = {
-    "Name" = "NAT - ${element(aws_availability_zones.available-azs, count.index)}"
+    "Name" = "NAT - ${element(data.aws_availability_zones.available-azs.names, count.index)}"
   }
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    count           = "${length(aws_availability_zones.available-azs)}"
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = "${element(aws_nat_gateway.main.*.id, count.index)}"
-  }
-
-  depends_on = ["${aws_internet_gateway.main.id}"]
+  count   = length(data.aws_availability_zones.available-azs.names)
+  vpc_id  = aws_vpc.main.id
 
   tags = {
-    "Name" = "Public Routing Table"
+    "Name" = "Private Routing Table"
   }
 }
 
+resource "aws_route" "private" {
+  count                       = length(data.aws_availability_zones.available-azs.names)
+  route_table_id              = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block      = "0.0.0.0/0"
+  nat_gateway_id              = element(aws_nat_gateway.main.*.id, count.index)
+}
+
 resource "aws_route_table_association" "private" {
-  subnet_id       = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id  = "${aws_route_table.private.id}"
+  count           = length(data.aws_availability_zones.available-azs.names)
+  subnet_id       = element(aws_subnet.private.*.id, count.index)
+  route_table_id  = element(aws_route_table.private.*.id, count.index)
 }
